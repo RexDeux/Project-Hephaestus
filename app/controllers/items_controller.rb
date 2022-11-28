1abaf9777c6a57b3f8d41f94bf17979f99bc550b
Nuno <nm.cdc@hotmail.com>
2022-11-28 22:23:44 +0000

    first page built, added basic functions to controllers

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,2 +1,63 @@
 class ItemsController < ApplicationController
+  before_action :set_items, only: %i[show edit update destroy]
+
+  def home
+  end
+
+
+  def index
+    @items = Item.all
+  end
+
+  def show
+  end
+
+  def new
+    @item = Item.new
+  end
+
+  def edit
+  end
+
+  def create
+    @item = Item.new(item_params)
+    respond_to do |format|
+      if @item.save
+        format.html { redirect_to @item, notice: 'item was successfully created.' }
+        format.json { render :show, status: :created, location: @item }
+      else
+        format.html { render :new }
+        format.json { render json: @item.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  def update
+    respond_to do |format|
+      if @item.update(item_params)
+        format.html { redirect_to @item, notice: 'item was successfully updated.' }
+        format.json { render :show, status: :ok, location: @item }
+      else
+        format.html { render :edit }
+        format.json { render json: @item.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  def destroy
+    @item.destroy
+    respond_to do |format|
+      format.html { redirect_to items_url, notice: 'item was successfully destroyed.' }
+      format.json { head :no_content }
+    end
+  end
+
+  private
+  def item_params
+    params.require(:item).permit(:item_name, :price, :photo, :description, :quantity)
+  end
+
+  def set_item
+    @item = item.find(params[:id])
+  end
 end
