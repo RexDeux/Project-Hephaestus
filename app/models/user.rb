4a4fe76d0dbfa2087b8a13288bb28e021dd3fad7
Nuno Costa <nm.cdc@hotmail.com>
2024-07-09 13:28:25 +0100

    new user creation was broken, fixed it

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,11 +3,13 @@ class User < ApplicationRecord
     has_one :cart, dependent: :destroy
     has_secure_password
 
-    after_create :create_cart_for_user
+    after_create :create_cart
 
     private
 
     def create_cart
+        @user = User.new
+        @user.save
         Cart.create(user: self)
     end
 end
